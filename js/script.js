f083de5d3d09bd1d4fadf8dd1d8727713124378c
numonof <islomxonnumonov3701@gmail.com>
2022-10-28 19:08:23 +0300

    change css & js structure and Mediaquery

--- js/script.js
+++ js/script.js
@@ -1,178 +1,144 @@
-'use strict'
-let formCreate = document.getElementById("form-create");
-let formEdit = document.getElementById("form-edit");
-let listGroupTodo = document.getElementById("list-group-todo");
-let messageCreate = document.getElementById("message-create");
-let time = document.getElementById("time");
-
-//modal
-let modal = document.querySelector('#modal');
-let owerlay = document.querySelector('#owerlay');
-let closeBtn = document.querySelector('.close-modal')
-//time elements
+"use strict";
+/*Header*/
 let fullDay = document.getElementById("full-day");
 let hourEl = document.getElementById("hour");
-let minuteEl = document.getElementById("minute");
-let secondEl = document.getElementById("second");
+let minutsEl = document.getElementById("minute");
+let secondsEl = document.getElementById("second");
+
+/*Todo Elements*/
+let creatForm = document.getElementById("form-create");
+let inputCreate = document.getElementById("input-create");
+let messageCreate = document.getElementById("message-create");
+let createBtn = document.getElementById("create-btn");
+let todoList = document.getElementById("list-group-todo");
+
+/* Edit Todo Elements*/
 let editItemId;
-//chek
-let todos = JSON.parse(localStorage.getItem("list"))
-  ? JSON.parse(localStorage.getItem("list"))
-  : [];
-
-if (todos.length){
- showTodos();
-}  
-
-//error Message
+let Modal = document.getElementById("modal");
+let closeBtn = document.querySelector(".close-modal");
+let editForm = document.getElementById("form-edit");
+let editInput = document.getElementById("input-edit");
+let messagEdit = document.getElementById("message-edit");
+let owerlay  = document.getElementById("owerlay")
+/* Data and Time*/
+function getDate() {
+  const now = new Date();
+  /* console.log(now);*/
+  /*Date*/
+  function dateNow() {
+    const day = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
+    const month = now.getMonth() < 10 ? "0" + (now.getMonth() + 1) : now.getMonth();
+    const year = now.getFullYear();
+    /* console.log(+day, +month, year);*/
+    const months = [
+      "Jenuary",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const monthsTitle = now.getMonth();
+    fullDay.textContent = `${+day} ${months[monthsTitle]} ${year}`;  
+  return `${day}.${+month + 1}.${year}`
+  };
+  dateNow();
+  /* console.log(dateNow());*/ 
+   /*Time*/
+  function time() {
+    const hour = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+    const minute =
+      now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+    const second =
+      now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+    const times = `${hour} : ${minute}`;
+    hourEl.textContent = hour;
+    minutsEl.textContent = minute;
+    secondsEl.textContent = second;
+    return times;
+  }
+  time();
+  /* console.log(time());*/
+  return `${time()} ${dateNow()}`;
+}
+setInterval(getDate, 100);
+getDate();
+/* console.log(getDate());*/
+/*Check todos*/
+let todos = JSON.parse(localStorage.getItem('list')) ? JSON.parse(localStorage.getItem('list')):[];
+// console.log(todos);
+if(todos.length){
+showTodos()
+}
+// get Todos
+creatForm.addEventListener('submit', (e)=>{
+   e.preventDefault();
+   const textContent = creatForm['input-create'].value.trim();
+   creatForm.reset()
+   console.log(textContent);
+  if (textContent.length) {
+    todos.push(
+      {
+        text:textContent,
+        time: getDate(),
+        completed:false,
+      })
+      setTodos()
+      showTodos()
+      // console.log(todos);
+  }else{
+    showMessage('message-create', "Please, Enter some text...");
+  }
+});
+//errorMessage
 function showMessage(where, message) {
   document.getElementById(`${where}`).textContent = message;
-  setTimeout(() => {
-    document.getElementById(`${where}`).textContent = " ";
-  }, 2000);
+  setTimeout(()=>{
+  document.getElementById(`${where}`).textContent = "";
+  }, 2500)
 }
-// set Todos to localStorage
+ /*set Todos to localStorage*/
 function setTodos() {
   localStorage.setItem("list", JSON.stringify(todos));
 }
-// Date Time
-function getTime() {
-  // Date
-  const now = new Date();
-  const day = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
-  const month = now.getMonth() < 10 ? "0" + now.getMonth() : now.getMonth();
-  const year = now.getFullYear();
-  //Time
-  const hour = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
-  const minute =
-    now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
-  const second =
-    now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
-  //Full Day Top
-  const months = [
-    "Jenuary",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  hourEl.textContent = hour;
-  minuteEl.textContent = minute;
-  secondEl.textContent = second;
-  const monthTitle = now.getMonth();
-  fullDay.textContent = ` ${day} ${months[monthTitle]} ${year}`;
-  // return
-  return `${hour}:${minute} ${day}.${+month + 1}.${year}`;
-}
-// console.log(getTime());
-setInterval(getTime, 100);
-// show Todos
+/* show Todo List*/
 function showTodos() {
-  const todosList = JSON.parse(localStorage.getItem("list"));
-  listGroupTodo.innerHTML = "";
-  todosList.forEach((element, index) => {
-    listGroupTodo.innerHTML += `
-      <li ondblclick="setCompleted(${index})" class="list-group__item" id="${
-        element.completed == true ?'completed' : ''
-      }">
-         ${element.text}
-         <div class="todo-icons">
-            <div class="date">${element.time}</div>
-              <div class="icons" onclick=(editTodos(${index}))>
-                <img src="./img/pencil.png" alt="edit icon" onclick=(editTodo(${index})) width="20" height="20">
-              </div>
-              <div class="icons" onclick=(deleteTodo(${index}))>
-                <img src="./img/delete.png" alt="delete icon" width="20" height="20">
-              </div>
-          </div>
-        </li>
-
-     `;
-  });
-}
-// get Todos
-formCreate.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const textTodos = formCreate["input-create"].value.trim();
-  formCreate.reset();
-  if (textTodos.length) {
-    todos.push(
-      { 
-      text: textTodos,
-      time: getTime(),
-      completed: false 
-    });
-    setTodos();
-    showTodos();
-    console.log(todos);
-    console.log(textTodos);
-  } else {
-    showMessage("message-create", "Please, Enter some text...");
-  }
-});
-
-// delete Todo
+  const todosListItem = JSON.parse(localStorage.getItem('list'));
+  todoList.innerHTML = ''
+     todosListItem.forEach((element, index) => {
+  // console.log(element, index); 
+  todoList.innerHTML += `
+        <li ondblclick="setCompleted(${index})" class="list-group__item" id="${
+          element.completed == true ?'completed' : ''
+        }" >
+           ${element.text}
+           <div class="todo-icons">
+              <div class="date">${element.time}</div>
+                <div class="icons" onclick=(editTodos(${index}))>
+                  <img src="./img/pencil.png" alt="edit icon" onclick=(editTodo() width="20" height="20">
+                </div>
+                <div class="icons" onclick=(deleteTodo(${index}))>
+                  <img src="./img/delete.png" alt="delete icon" width="20" height="20">
+                </div>
+            </div>
+          </li>`   
+     });
+};
+// showTodos();
+// Delete Todo List
 function deleteTodo(id) {
-  console.log(id);
-  const DeletedTodos = todos.filter((item, index) => {
-    return index !== id;
-  });
-  todos = DeletedTodos;
+  const DeletedTodoList = todos.filter((item, index)=>{
+    return id !== index 
+  })
+  todos = DeletedTodoList;
   setTodos();
   showTodos();
-}
-
-//open modal
-function openModal(){
-  modal.classList.remove('hidden')
-  owerlay.classList.remove('hidden')
-}
-//formEdit
-formEdit.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const editTodos = formEdit["input-edit"].value.trim();
-  formEdit.reset();
-  if (editTodos.length) {
-    todos.splice(
-      editItemId, 1, 
-       {
-       text: editTodos,
-       time: getTime(), 
-       completed: false 
-      });
-
-    setTodos();
-    showTodos();
-    closeModal()
-  } else {
-    showMessage("message-edit", "Please, Enter some text...");
-  }
-});
-
-//edit Todos
-function editTodos(id) {
-  openModal()
-  editItemId = id
-  console.log(editItemId);
-}
-
-//close modal
-closeBtn.addEventListener('click', ()=>{
-  closeModal()
-});
-owerlay.addEventListener('click', ()=>{
-  closeModal()
-})
-function closeModal(){
-  modal.classList.add('hidden')
-  owerlay.classList.add('hidden')
 }
 // setCompleted
 function setCompleted(id) {
@@ -187,4 +153,59 @@ function setCompleted(id) {
   setTodos();
   showTodos();
 }
-console.log(todos);
+
+//open edit form
+function openModal(){
+  Modal.classList.remove('hidden')
+  owerlay.classList.remove('hidden')
+}
+//close modal
+function closeModal(){
+  Modal.classList.add('hidden')
+  owerlay.classList.add('hidden')
+}
+closeBtn.addEventListener('click', ()=>{
+  closeModal()
+});
+owerlay.addEventListener('click', ()=>{
+  closeModal()
+});
+//formEdit
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const editTodos = editForm['input-edit'].value.trim();
+    editForm.reset();
+if (editTodos.length) {
+  todos.splice(editItemId , 1 , {
+    text:editTodos,
+    time: getDate(), 
+    completed: false, 
+  });
+  setTodos();
+  showTodos();
+  closeModal();
+} else {
+    showMessage("message-edit", "Please, Enter some text...");
+};
+//   console.log(editTodos);
+//   if (editTodos.lenght) {
+//     todos.splice(editItemId, 1, {
+//        text: editTodos,
+//        time: getTime(), 
+//        completed: false 
+//       });
+// // console.log(todos);
+//     setTodos();
+//     showTodos();
+//     closeModal()
+//   } else {
+//     showMessage("message-edit", "Please, Enter some text...");
+//   }
+});
+//edit Todos
+function editTodos(id) {
+  console.log(id);
+  openModal()
+  editItemId = id;
+  // console.log(editItemId);
+}
